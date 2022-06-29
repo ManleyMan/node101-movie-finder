99faf2a25ef698963f271e36bcb23aff6fc75953
ManleyMan <sweezymac@gmail.com>
2022-06-29 16:02:03 -0700

    created dotenv file

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+app.use(morgan('dev'));
 
+let movieData = [];
+
+// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+app.get('/', (req,res) => {
+    console.log('its working')
+})
 module.exports = app;
